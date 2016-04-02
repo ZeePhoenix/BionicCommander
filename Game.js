0afa56e5ea044e0f8514b004c4cb0fccc1c0ec0c
Zachary Cupec <zcupec@gmail.com>
2016-04-02 08:16:13 -0700

    Testing index page

--- Game.js
+++ Game.js
@@ -11,3 +11,27 @@
 //       Item drops
 // ==================================
 
+Bionic.Game = function(game) {
+    
+    
+    this._player = null;
+    
+    
+};
+
+Bionic.Game.prototype = {
+    create: function() {
+        this.physics.startSystem(Phaser.physics.ARCADE);
+        this.physics.ARCADE.gravity.y = 200;
+        
+        this.add.sprite(0, 0, 'background');
+        
+        this._player = this.add.sprite(5, 500, 'radSpencer');
+        this._player.animations.add('idle');
+        
+        
+    },
+    update: function() {
+        
+    }
+};
