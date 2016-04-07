eaf43e51dabbbebb9f844259ac4faf5b21bebb87
Zachary Cupec (RIT Student) <zac2275@ad.rit.edu>
2016-04-06 20:06:07 -0400

    Moved Player code to it's own "class"

--- Game.js
+++ Game.js
@@ -34,88 +34,20 @@ Bionic.MainGame = function(game) {
     this.cursors;
     this.shoot;
     this.c_shoot;
+    
 };
 
 Bionic.MainGame.prototype = {
-    create: function() {
+    create: function(game) {
         // Set up game world
-        this.physics.startSystem(this.Phaser.Physics.ARCADE);
+        this.physics.startSystem(this.physics.ARCADE);
         this.physics.gravity = true;
         this.physics.arcade.gravity.y = 250;
         
         // Set up map
         this.background = this.add.sprite(-1500, -500, 'background');
         
-        // Set up Player
-        this.player = this.add.sprite(25, 350, 'radSpencer');
-        this.player.prototype = {
-            // Animation
-            idle: undefined,
-            walk: undefined,
-            crouch: undefined,
-            c_crouch: undefined,
-            c_diag: undefined,
-            c_up: undefined,
-            swing: undefined,
-            hurt: undefined,
-            dead: undefined,
-            currState: undefined,
-            moveSpeed: undefined,
-            // Claw
-            claw: undefined,
-            facing: 'right',
-            bullets: undefined,
-            
-            updateAnimation: function(){
-                switch(this.currState){
-                    case this.STATE.IDLE:
-                        this.player.animations.play('idle');
-                        break;
-                    case this.STATE.WALK:
-                        this.player.animations.play('walk', 5, true);
-                        break;
-                }
-            }
-        };
-        this.player.claw = this.add.sprite(-200, -200, 'clawSheet');
-        this.player.claw.prototype = {
-            firing: false,
-            hooked: false,
-            extend: undefined,
-            currState: undefined
-        };
-        this.player.bullets = this.add.group();
-        this.player.bullets.enableBody = true;
-        this.player.bullets.physicsBodyType = this.Phaser.Physics.ARCADE;
-        this.player.bullets.createMultiple(30, 'bullet');
-        this.player.bullets.setAll('anchor.x', 0.5);
-        this.player.bullets.setAll('anchor.y', 1);
-        this.player.bullets.setAll('outOfBoundsKill', true);
-        this.player.bullets.setAll('checkWorldBounds', true);
-        this.player.bullets.setAll('body.allowGravity', false);
-        this.game.physics.enable(this.player, this.Phaser.ARCADE);
-        this.player.body.collideWorldBounds = true;
-        this.player.currState = this.STATE.IDLE;
-        this.player.moveSpeed = 200;
-        Bionic.health = 1;
-        this.player.scale.x = this.player.scale.y = 2;
-        // Player animations
-        this.player.anchor.setTo(0.5, 0.5);
-        this.player.idle = this.player.animations.add('idle', [0]);
-        this.player.walk = this.player.animations.add('walk', [1, 2, 3]);
-        this.player.crouch = this.player.animations.add('crouch', [4]);
-        this.player.c_crouch = this.player.animations.add('c_crouch', [5, 6]);
-        this.player.c_diag = this.player.animations.add('c_diag', [7, 8]);
-        this.player.c_up = this.player.animations.add('c_up', [9, 10]);
-        this.player.swing = this.player.animations.add('swing', [11, 12]);
-        this.player.hurt = this.player.animations.add('hurt', [13]);
-        // Player's Claw
-        this.physics.arcade.enable(this.player.claw);
-        this.player.claw.body.allowGravity = false;
-        this.player.claw.extend = this.player.claw.animations.add(0, [0, 1, 2, 1, 0]);
-        this.player.claw.anchor.setTo(0.5, 0.5);
-        this.player.claw.currState = this.C_STATE.NONE;
-        this.player.claw.scale.x = this.player.claw.y = 3;
+        Bionic.Player.spawnPlayer(game, 100, 450, 'radSpencer', 'clawSheet');
         
         
         // Enemy
@@ -150,51 +82,54 @@ Bionic.MainGame.prototype = {
         
         
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.shoot = this.input.keyboard.addKey(this.Phaser.Keyboard.x);
-        this.c_shoot = this.input.keyboard.addKey(this.Phaser.Keyboard.z);
+        this.shoot = this.input.keyboard.addKey(Phaser.Keyboard.x);
+        this.c_shoot = this.input.keyboard.addKey(Phaser.Keyboard.x);
     },
     update: function() {
         this.checkControls();
         
-        if (this.player.body.velocity.y > 200) this.player.currState = this.STATE.PARACHUTE;
         
-        switch (this.player.currState)
+        switch (Bionic.Player.currState)
         {
             case this.STATE.IDLE:
-                this.player.animations.play('idle');
-                this.player.body.velocity.x = 0;
+                Bionic.Player.sprite.animations.play('idle');
+                Bionic.Player.sprite.body.velocity.x = 0;
                 break;
             case this.STATE.WALK:
-                this.player.animations.play('walk', 10, true);
-                switch (this.player.facing){
+                Bionic.Player.sprite.animations.play('walk', 10, true);
+                switch (Bionic.Player.facing){
                     case 'right':
-                        this.player.body.velocity.x = this.player.moveSpeed;
+                        Bionic.Player.sprite.body.velocity.x = Bionic.Player.moveSpeed;
                         break;
                     case 'left':
-                        this.player.body.velocity.x = -this.player.moveSpeed;
+                        Bionic.Player.sprite.body.velocity.x = -Bionic.Player.moveSpeed;
                 }
                 break;
             case this.STATE.CROUCH:
-                this.player.animations.play('crouch', 10, true);
-                this.player.body.velocity.x = 0;
+                Bionic.Player.sprite.animations.play('crouch', 10, true);
+                Bionic.Player.sprite.body.velocity.x = 0;
                 break;
             case this.STATE.C_CROUCH:
+                if (!this.player.claw.firing) this.fireClaw();
+                break;
             case this.STATE.C_DIAG:
+                if (!this.player.claw.firing) this.fireClaw();
+                break;
             case this.STATE.C_UP:
                 if (!this.player.claw.firing) this.fireClaw();
                 break;
                 
         }
         
-        switch (this.player.facing)
+        switch (Bionic.Player.facing)
         {
             case 'right':
-                this.player.scale.x = 2;
-                this.player.scale.y = 2;
+                Bionic.Player.sprite.scale.x = 2;
+                Bionic.Player.sprite.scale.y = 2;
                 break;
             case 'left':
-                this.player.scale.x = -2;
-                this.player.scale.y = 2;
+                Bionic.Player.sprite.scale.x = -2;
+                Bionic.Player.sprite.scale.y = 2;
                 break;
         }
     },
@@ -202,24 +137,30 @@ Bionic.MainGame.prototype = {
     checkControls: function() {
         // Check for movement
         if (this.cursors.down.isDown){
-            this.player.currState = this.STATE.CROUCH;
-            if (this.cursors.left.isDown){
-                this.player.facing = 'left';
+            if (this.c_shoot.isUp){
+                Bionic.Player.updateState(this.STATE.C_CROUCH);
             }
-            else if (this.cursors.right.isDown){
-                this.player.facing = 'right';
+            else {
+                Bionic.Player.updateState(this.STATE.CROUCH);
+                if (this.cursors.left.isDown){
+                    Bionic.Player.facing = 'left';
+                }
+                else if (this.cursors.right.isDown){
+                    Bionic.Player.facing = 'right';
+                }
             }
+            
         }
         else {
             if (this.cursors.left.isDown){
-                this.player.facing = 'left';
-                this.player.currState = this.STATE.WALK;
+                Bionic.Player.facing = 'left';
+                Bionic.Player.updateState(this.STATE.WALK);
             }
             else if (this.cursors.right.isDown){
-                this.player.facing = 'right';
-                this.player.currState = this.STATE.WALK;
+                Bionic.Player.facing = 'right';
+                Bionic.Player.updateState(this.STATE.WALK);
             }
-            else this.player.currState = this.STATE.IDLE;
+            else Bionic.Player.updateState(this.STATE.IDLE);
         }
         
     },
@@ -278,6 +219,99 @@ Bionic.MainGame.prototype = {
             this.player.claw.x = -200;
             this.player.claw.y = -200;
             this.player.claw.firing = false;
+            
         });
     }
+};
+
+Bionic.Player = {
+    // Player Properties
+    sprite: undefined,
+    facing: 'right',
+    scale: 2,
+    currState: undefined,
+    moveSpeed: undefined,
+    currHealth: undefined,
+    maxHealth: undefined,
+    claw: undefined,
+    
+    animationList: {
+        idle: undefined,
+        walk: undefined,
+        crouch: undefined,
+        c_crouch: undefined,
+        c_diag: undefined,
+        c_up: undefined,
+        swing: undefined,
+        hurt: undefined,
+        dead: undefined,
+    },
+
+    spawnPlayer: function(game, x, y, sprite, clawSprite){
+        // Health / Damage
+        this.maxHealth = this.currHealth = 4;
+        // Game Sprite
+        this.sprite = game.add.sprite(x, y, sprite);
+        this.animationList.idle = this.sprite.animations.add('idle', [0]);
+        this.animationList.walk = this.sprite.animations.add('walk', [1, 2, 3]);
+        this.animationList.crouch = this.sprite.animations.add('crouch', [4]);
+        this.animationList.c_crouch = this.sprite.animations.add('c_crouch', [5, 6]);
+        this.animationList.c_diag = this.sprite.animations.add('c_diag', [7, 8]);
+        this.animationList.c_up = this.sprite.animations.add('c_up', [9, 10]);
+        this.animationList.swing = this.sprite.animations.add('swing', [11, 12]);
+        this.animationList.hurt = this.sprite.animations.add('hurt', [13]);
+        this.sprite.anchor.setTo(0.5, 0.5);
+        // Physics
+        game.physics.enable(this.sprite, game.physics.ARCADE);
+        this.sprite.body.collideWorldBounds = true;
+        this.moveSpeed = 200;
+        // Claw Initialization
+        this.claw = game.add.sprite(-200, -200, clawSprite);
+        this.claw.prototype = {
+            animation: this.sprite.animations.add('animation', [0, 1, 2, 1, 0]),
+            firing: false,
+            hooked: false,
+        }
+    },
+        
+    updateState: function(animationState){
+        this.currState = animationState;
+    },
+
+    /*    this.player.claw = this.add.sprite(-200, -200, 'clawSheet');
+        this.player.claw.prototype = {
+            firing: false,
+            hooked: false,
+            extend: undefined,
+            currState: undefined
+        };
+        this.player.bullets = this.add.group();
+        this.player.bullets.enableBody = true;
+        this.player.bullets.physicsBodyType = this.physics.ARCADE;
+        this.player.bullets.createMultiple(30, 'bullet');
+        this.player.bullets.setAll('anchor.x', 0.5);
+        this.player.bullets.setAll('anchor.y', 1);
+        this.player.bullets.setAll('outOfBoundsKill', true);
+        this.player.bullets.setAll('checkWorldBounds', true);
+        this.player.bullets.setAll('body.allowGravity', false);
+
+        this.player.scale.x = this.player.scale.y = 2;
+        // Player animations
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.idle = this.player.animations.add('idle', [0]);
+        this.player.walk = this.player.animations.add('walk', [1, 2, 3]);
+        this.player.crouch = this.player.animations.add('crouch', [4]);
+        this.player.c_crouch = this.player.animations.add('c_crouch', [5, 6]);
+        this.player.c_diag = this.player.animations.add('c_diag', [7, 8]);
+        this.player.c_up = this.player.animations.add('c_up', [9, 10]);
+        this.player.swing = this.player.animations.add('swing', [11, 12]);
+        this.player.hurt = this.player.animations.add('hurt', [13]);
+        // Player's Claw
+        this.physics.arcade.enable(this.player.claw);
+        this.player.claw.body.allowGravity = false;
+        this.player.claw.extend = this.player.claw.animations.add(0, [0, 1, 2, 1, 0]);
+        this.player.claw.anchor.setTo(0.5, 0.5);
+        this.player.claw.currState = this.C_STATE.NONE;
+        this.player.claw.scale.x = this.player.claw.y = 3;
+    */
 };
